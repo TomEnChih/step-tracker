a9444322f63ed46d88ecfe7b565856746f524e4f
TomEnChih <tom10485916@gmail.com>
2025-08-06 02:38:28 +0800

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Tom Tung on 2025/8/6.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
